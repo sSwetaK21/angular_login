cef4f2b54f2da2a932cd99dfe8012bf181aef529
Shweta Kodam <shweta.kodam@ecotechservices.com>
2024-02-26 19:18:03 +0530

    error

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,28 +1,39 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  
-  constructor( private toastr: ToastrService,
-    private authService: AuthService, private router:Router){ }
+  constructor(
+    private toastr: ToastrService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-   
-   if(this.authService.isLoggedIn()){
-    return true
-   }else{
-    this.router.navigate(['/login'])
-    return false
-   }
-
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      alert('You donnt have authorisation');
+      return false;
+    }
   }
-
 }
